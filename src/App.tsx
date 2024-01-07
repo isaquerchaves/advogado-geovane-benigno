5fa8531a71b687a3de8819680d1978f3a704a88a
Isaque Ramos Chaves <isaque.chaves@escolaconquer.com.br>
2024-01-07 16:06:02 -0300

    adicionado card Entenda

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChevronsDown, MapPinned, ScreenShare, ShieldCheck, Star } from 'lucide-react'
+import { ChevronsDown, MapPinned, Minus, ScreenShare, ShieldCheck, Star } from 'lucide-react'
 import './App.css'
 import Button from './components/button/button'
 
@@ -40,9 +40,20 @@ function App() {
           <ChevronsDown size={42} />
           <p style={{ borderLeft: '2px solid black', width: '100%', padding: '10px' }}>Se você foi vítima de um <span style={{ fontWeight: 'bold' }}>golpe do Pix</span> ou conhece alguém próximo que sofreu com este crime, não exite em contactar. Sou especialista em fraudes bancárias e estou sempre disposto a ajudar.</p>
         </div>
-      </section>
-      <section className='section-2'>
-
+        <div className='card-section-2'>
+          <div className='card-section-2-content'>
+            <div className='entenda'>
+              <Minus />
+              <p>ENTENDA</p>
+            </div>
+            <h1 className='card-section-2-title'>Escritório de Advocacia Especializado em Golpe do Pix</h1>
+            <div>
+              <p className='card-section-2-text'>Há duas coisas que você deve fazer imediatamente ao sofrer um golpe do Pix. Em primeiro lugar, ligue para seu banco e informe o quanto antes. Existem mecanismos bancários que podem bloquear o valor, dependendo da situação. Nestes casos, você ganha tempo para realizar as averiguações necessárias.</p>
+              <p className='card-section-2-text'>Além disso, é claro, você deve entrar em contato com um escritório de advocacia especialista. Desta forma, você já inicia os procedimentos de Boletim de Ocorrência e dá início às ações necessárias para recuperar o dinheiro e processar o estelionatário pelos crimes cometidos. Neste tipo de situação, a agilidade é fundamental.</p>
+            </div>
+          </div>
+          <img src="golpe-do-pix.jpg" alt="Golpe do Pix" className='card-section-2-img' />
+        </div>
       </section>
     </>
   )
