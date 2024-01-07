9f770ae421a70be79b2d236009da6e0eef6ccbad
Isaque Ramos Chaves <isaque.chaves@escolaconquer.com.br>
2024-01-07 13:05:06 -0300

    configuração do botão do whatsapp

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,8 +1,14 @@
+import React from 'react';
 import './button.css'
 
 const Button = () => {
+    const handleClick = () => {
+        const whatsappLink = "https://api.whatsapp.com/send?phone=559992285423&text=busco orientação profissional"
+        window.location.href = whatsappLink;
+    }
+
     return (
-        <button className='button'>
+        <button className='button' onClick={handleClick}>
             <div className='button-content'>
                 <img src='/whatsapp.png' className='whatsapp' />
                 <p>QUERO ENTRAR EM CONTATO</p>
